2a3388180be01b1784c15a554e993c3514ac4c38
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-07-20 18:53:29 +0200

    Add basic Localization traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-use fluent::FluentBundle;
-
+use crate::fluent::FluentResource;
 use bevy::{prelude::*, reflect::TypeUuid};
-use fluent::FluentResource;
 use loaders::ftl_loader::FtlLoader;
 use unic_langid::LanguageIdentifier;
 
+mod fluent;
 mod loaders;
+pub mod localization;
 
 #[derive(PartialEq, Eq, Debug, Clone)]
 /// The currently active locale
@@ -37,54 +37,6 @@ impl LocalizationSource {
 #[derive(PartialEq, Eq)]
 pub struct LocalizationError;
 
-// TODO: Make a fancy derive macro for this
-pub trait LocalizationBundle {
-    /// Try to get the localization resource handle for the given language ID.
-    fn try_get_resource_handle(
-        &self,
-        language_id: &LanguageIdentifier,
-    ) -> Result<Handle<LocalizationSource>, LocalizationError>;
-
-    fn try_get_message(
-        &self,
-        current_locale: &CurrentLocale,
-        assets: Res<Assets<LocalizationSource>>,
-        message_id: &str,
-    ) -> Result<String, LocalizationError> {
-        let language_id = &current_locale.0;
-        let handle = self.try_get_resource_handle(language_id)?;
-
-        // Build resource
-        let resource = if let Some(source) = assets.get(&handle) {
-            Ok(&source.resource)
-        } else {
-            Err(LocalizationError)
-        }?;
-
-        // Build bundle
-        let mut bundle = FluentBundle::new(vec![language_id.clone()]);
-
-        if bundle.add_resource(resource).is_err() {
-            return Err(LocalizationError);
-        }
-
-        // Get message
-        let msg = bundle
-            .get_message(message_id)
-            .expect("Message doesn't exist.");
-
-        let mut errors = vec![];
-        let pattern = msg.value().expect("Message has no value.");
-        let value = bundle.format_pattern(pattern, None, &mut errors);
-
-        if !errors.is_empty() {
-            Err(LocalizationError)
-        } else {
-            Ok(value.to_string())
-        }
-    }
-}
-
 /// Idk what I'm doing
 pub struct LocalizationOutput;
 
@@ -96,7 +48,7 @@ impl FromWorld for LocalizationOutput {
 }
 
 #[derive(Default)]
-pub struct LocalizationPlugin;
+pub struct LocalizationPlugin {}
 
 impl Plugin for LocalizationPlugin {
     fn build(&self, app: &mut App) {
---
+++ src/localization.rs
@@ -0,0 +1,71 @@
+use std::marker::PhantomData;
+
+use crate::fluent::FluentBundle;
+use bevy::prelude::*;
+
+use crate::LocalizationError;
+
+// TODO: Review if the 'static is really needed for world.contains_resource
+pub trait LocalizationFolder: 'static + std::marker::Send + std::marker::Sync {
+    fn folder_path() -> String;
+}
+
+pub struct Localization<T: LocalizationFolder> {
+    phantom: std::marker::PhantomData<T>,
+    cur_bundle: Option<FluentBundle>,
+}
+
+impl<T: LocalizationFolder> Localization<T> {
+    pub fn new() -> Self {
+        Self {
+            phantom: PhantomData,
+            cur_bundle: None,
+        }
+    }
+
+    pub fn try_get_message(&self, message_id: &str) -> Result<String, LocalizationError> {
+        let bundle = if let Some(bundle) = &self.cur_bundle {
+            bundle
+        } else {
+            return Err(LocalizationError);
+        };
+
+        let msg = bundle
+            .get_message(message_id)
+            .expect("Message doesn't exist.");
+
+        let mut errors = vec![];
+        let pattern = msg.value().expect("Message has no value.");
+        let value = bundle.format_pattern(pattern, None, &mut errors);
+
+        if !errors.is_empty() {
+            Err(LocalizationError)
+        } else {
+            Ok(value.to_string())
+        }
+    }
+}
+
+impl<T: LocalizationFolder> Default for Localization<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+pub trait AddLocalization {
+    fn add_localization<T: LocalizationFolder>(&mut self) -> &mut Self;
+}
+
+impl AddLocalization for App {
+    fn add_localization<T: LocalizationFolder>(&mut self) -> &mut Self {
+        if self.world.contains_resource::<Localization<T>>() {
+            return self;
+        }
+
+        let localization = Localization::<T>::new();
+
+        self.insert_resource(localization);
+
+        self
+    }
+}
