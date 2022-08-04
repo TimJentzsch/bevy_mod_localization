4d436024d89bb0b1e159572ae33347acca7e9ba5
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-04 11:06:27 +0200

    Enable hot reloading for basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,4 +1,7 @@
-use bevy::{asset::AssetPlugin, prelude::*};
+use bevy::{
+    asset::{AssetPlugin, AssetServerSettings},
+    prelude::*,
+};
 use bevy_prototype_fluent::{
     localization::AddLocalization,
     localization::{Localization, LocalizationFolder},
@@ -18,11 +21,19 @@ impl LocalizationFolder for ExampleLocalization {
 
 fn main() {
     App::new()
+        // Optional: Enable hot reloading
+        .insert_resource(AssetServerSettings {
+            watch_for_changes: true,
+            ..default()
+        })
+        // Define the locale that you want to use by default
         .insert_resource(CurrentLocale::new(langid!("en-US")))
         .add_plugins(MinimalPlugins)
         .add_plugin(AssetPlugin)
         .add_plugin(LocalizationPlugin)
+        // Add the localization resource for the given folder
         .add_localization::<ExampleLocalization>()
+        // Do something with it!
         .add_system(use_localization_system)
         .run();
 }
