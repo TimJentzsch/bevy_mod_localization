26e9a7f1d24ce8d2d1e515b1e6d6755f47bc56d9
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-12 15:04:21 +0200

    Implement fallback mechanism for localization

--- src/localization.rs
+++ src/localization.rs
@@ -3,9 +3,15 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use crate::{fluent::FluentBundle, locale::Locale, plugin::LocalizationStage, LocalizationSource};
-use bevy::prelude::*;
+use crate::{
+    fluent::FluentBundle,
+    locale::{Locale, LocaleDefaultFallback, LocaleFallbackMap},
+    plugin::LocalizationStage,
+    LocalizationSource,
+};
+use bevy::{prelude::*, utils::HashMap};
 use fluent::{FluentArgs, FluentResource};
+use unic_langid::LanguageIdentifier;
 
 use crate::LocalizationError;
 
@@ -47,16 +53,16 @@ pub trait LocalizationFolder: 'static + std::marker::Send + std::marker::Sync {
 
 pub struct Localization<T: LocalizationFolder> {
     phantom: std::marker::PhantomData<T>,
-    handle: Handle<LocalizationSource>,
-    cur_bundle: Option<FluentBundle>,
+    pub(crate) handle_map: HashMap<LanguageIdentifier, Handle<LocalizationSource>>,
+    pub(crate) bundle_map: HashMap<LanguageIdentifier, FluentBundle>,
+    pub(crate) resolution_chain: Vec<LanguageIdentifier>,
 }
 
 impl<T: LocalizationFolder> Localization<T> {
-    pub fn new(handle: Handle<LocalizationSource>) -> Self {
+    pub fn new(resolution_chain: Vec<LanguageIdentifier>) -> Self {
         Self {
-            phantom: PhantomData,
-            handle,
-            cur_bundle: None,
+            resolution_chain,
+            ..Default::default()
         }
     }
 
@@ -65,25 +71,22 @@ impl<T: LocalizationFolder> Localization<T> {
         message_id: &str,
         args: Option<&FluentArgs>,
     ) -> Result<String, LocalizationError> {
-        let bundle = if let Some(bundle) = &self.cur_bundle {
-            bundle
-        } else {
-            return Err(LocalizationError);
-        };
+        for lang_id in &self.resolution_chain {
+            if let Some(bundle) = self.bundle_map.get(lang_id)
+            && let Some(msg) = bundle.get_message(message_id) {
+                let mut errors = vec![];
 
-        let msg = bundle
-            .get_message(message_id)
-            .expect("Message doesn't exist.");
+                if let Some(pattern) = msg.value() {
+                    let formatted_message = bundle.format_pattern(pattern, args, &mut errors);
 
-        let mut errors = vec![];
-        let pattern = msg.value().expect("Message has no value.");
-        let value = bundle.format_pattern(pattern, args, &mut errors);
-
-        if !errors.is_empty() {
-            Err(LocalizationError)
-        } else {
-            Ok(value.to_string())
+                    if errors.is_empty() {
+                        return Ok(formatted_message.to_string());
+                    }
+                }
+            }
         }
+
+        Err(LocalizationError)
     }
 
     pub fn try_get_message(&self, message_id: &str) -> Result<String, LocalizationError> {
@@ -103,6 +106,17 @@ impl<T: LocalizationFolder> Localization<T> {
     }
 }
 
+impl<T: LocalizationFolder> Default for Localization<T> {
+    fn default() -> Self {
+        Self {
+            phantom: PhantomData,
+            resolution_chain: Vec::new(),
+            handle_map: HashMap::default(),
+            bundle_map: HashMap::default(),
+        }
+    }
+}
+
 pub trait AddLocalization {
     fn add_localization<T: LocalizationFolder>(&mut self) -> &mut Self;
 }
@@ -113,13 +127,22 @@ impl AddLocalization for App {
             return self;
         }
 
-        let locale = self.world.resource::<Locale>();
         let asset_server = self.world.resource::<AssetServer>();
 
-        let ftl_path = get_ftl_path::<T>(locale);
-        let handle: Handle<LocalizationSource> = asset_server.load(ftl_path);
+        let locale = self.world.resource::<Locale>();
+        let fallback_map = self.world.resource::<LocaleFallbackMap>();
+        let default_fallback = self.world.resource::<LocaleDefaultFallback>();
 
-        let localization = Localization::<T>::new(handle);
+        let resolution_chain = get_resolution_chain(locale, fallback_map, default_fallback);
+
+        let mut localization = Localization::<T>::new(resolution_chain.clone());
+
+        // Initiate loading of the localization files
+        for lang_id in resolution_chain {
+            let ftl_path = get_ftl_path::<T>(&lang_id);
+            let handle: Handle<LocalizationSource> = asset_server.load(ftl_path);
+            localization.handle_map.insert(lang_id.clone(), handle);
+        }
 
         self.insert_resource(localization)
             // First, check if the locale changed
@@ -139,22 +162,65 @@ impl AddLocalization for App {
     }
 }
 
-/// Get the path of the FTL file for the given locale in the localization folder.
-fn get_ftl_path<T: LocalizationFolder>(locale: &Locale) -> PathBuf {
-    Path::new(&T::FOLDER_PATH).join(format!("{}.ftl", locale.0))
+/// Get the path of the FTL file for the given language ID in the localization folder.
+fn get_ftl_path<T: LocalizationFolder>(lang_id: &LanguageIdentifier) -> PathBuf {
+    Path::new(&T::FOLDER_PATH).join(format!("{}.ftl", lang_id))
+}
+
+fn get_resolution_chain(
+    locale: &Locale,
+    fallback_map: &LocaleFallbackMap,
+    default_fallback: &LocaleDefaultFallback,
+) -> Vec<LanguageIdentifier> {
+    let locale = locale.0.clone();
+    let fallbacks = fallback_map.0.get(&locale);
+    let default_fallback = default_fallback.0.clone();
+
+    let mut resolution_chain = Vec::with_capacity(fallbacks.map_or(0, |f| f.len()) + 2);
+
+    resolution_chain.push(locale);
+
+    if let Some(fallbacks) = fallbacks {
+        resolution_chain.append(&mut fallbacks.clone());
+    }
+
+    if let Some(default_fallback) = default_fallback {
+        resolution_chain.push(default_fallback);
+    }
+
+    resolution_chain
 }
 
 /// Load the corresponding localization file when the locale has been changed.
 fn update_localization_on_locale_change<T: LocalizationFolder>(
     mut localization: ResMut<Localization<T>>,
     asset_server: ResMut<AssetServer>,
-    cur_locale: Res<Locale>,
+    locale: Res<Locale>,
+    fallback_map: Res<LocaleFallbackMap>,
+    default_fallback: Res<LocaleDefaultFallback>,
 ) {
-    if cur_locale.is_changed() {
-        let ftl_path = get_ftl_path::<T>(&cur_locale);
-        let handle: Handle<LocalizationSource> = asset_server.load(ftl_path);
+    if locale.is_changed() || fallback_map.is_changed() || default_fallback.is_changed() {
+        let resolution_chain = get_resolution_chain(&locale, &fallback_map, &default_fallback);
+        let handle_keys: Vec<LanguageIdentifier> =
+            localization.handle_map.keys().cloned().collect();
 
-        localization.handle = handle;
+        // Load handles that are now needed
+        for lang_id in resolution_chain
+            .iter()
+            .filter(|lang_id| !handle_keys.contains(lang_id))
+        {
+            let ftl_path = get_ftl_path::<T>(lang_id);
+            let handle = asset_server.load(ftl_path);
+            localization.handle_map.insert(lang_id.clone(), handle);
+        }
+
+        // Remove handles that are no longer needed
+        for lang_id in handle_keys
+            .iter()
+            .filter(|lang_id| !resolution_chain.contains(lang_id))
+        {
+            localization.handle_map.remove(lang_id);
+        }
     }
 }
 
@@ -168,38 +234,47 @@ fn update_localization_on_asset_change<T: LocalizationFolder>(
     mut localization: ResMut<Localization<T>>,
     mut ev_asset: EventReader<AssetEvent<LocalizationSource>>,
     assets: ResMut<Assets<LocalizationSource>>,
-    cur_locale: Res<Locale>,
 ) {
     for ev in ev_asset.iter() {
         match ev {
             AssetEvent::Created { handle } | AssetEvent::Modified { handle } => {
-                if *handle != localization.handle {
-                    continue;
+                let lang_id = localization.handle_map.iter().find_map(|(key, value)| {
+                    if *value == *handle {
+                        Some(key.clone())
+                    } else {
+                        None
+                    }
+                });
+
+                if let Some(lang_id) = lang_id {
+                    let source = assets
+                        .get(handle)
+                        .expect("Localization source expected to be loaded but it wasn't!");
+
+                    // TODO: Make this more efficient, the parsing could take some time
+                    let resource = FluentResource::try_new(source.ftl_string.clone())
+                        .expect("Failed to parse an FTL string.");
+
+                    let mut bundle = FluentBundle::new_concurrent(vec![lang_id.clone()]);
+                    bundle
+                        .add_resource(resource)
+                        .expect("Failed to add resource to bundle");
+
+                    localization.bundle_map.insert(lang_id.clone(), bundle);
                 }
-
-                let source = assets
-                    .get(handle)
-                    .expect("Localization source expected to be loaded but it wasn't!");
-
-                // TODO: Make this more efficient, the parsing could take some time
-                let resource = FluentResource::try_new(source.ftl_string.clone())
-                    .expect("Failed to parse an FTL string.");
-
-                let lang_id = cur_locale.0.clone();
-
-                let mut bundle = FluentBundle::new_concurrent(vec![lang_id]);
-                bundle
-                    .add_resource(resource)
-                    .expect("Failed to add resource to bundle");
-
-                localization.cur_bundle = Some(bundle);
             }
             AssetEvent::Removed { handle } => {
-                if *handle != localization.handle {
-                    continue;
-                }
+                let lang_id = localization.handle_map.iter().find_map(|(key, value)| {
+                    if *value == *handle {
+                        Some(key.clone())
+                    } else {
+                        None
+                    }
+                });
 
-                localization.cur_bundle = None;
+                if let Some(lang_id) = lang_id {
+                    localization.bundle_map.remove(&lang_id);
+                }
             }
         }
     }
