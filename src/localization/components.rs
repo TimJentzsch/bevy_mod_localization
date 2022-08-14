62548b0ed760bd498a96cdff3b5d8e8c5cb14da5
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-14 17:42:38 +0200

    Add documentation to LocalizedText

--- src/localization/components.rs
+++ src/localization/components.rs
@@ -4,6 +4,10 @@ use bevy::prelude::*;
 
 use super::LocalizationFolder;
 
+/// Automatically localize an entity with a [`Text`] component.
+///
+/// The first section of the [`Text`] component will be updated with the specified message.
+/// This will be updated every time the locale or localization file changes.
 #[derive(Component)]
 pub struct LocalizedText<T: LocalizationFolder> {
     phantom: PhantomData<T>,
@@ -11,6 +15,7 @@ pub struct LocalizedText<T: LocalizationFolder> {
 }
 
 impl<T: LocalizationFolder> LocalizedText<T> {
+    /// Create a new localzed text with the given message ID.
     pub fn new(message_id: &'static str) -> Self {
         Self {
             phantom: PhantomData,
@@ -18,6 +23,7 @@ impl<T: LocalizationFolder> LocalizedText<T> {
         }
     }
 
+    /// Get the message ID of the localized text.
     pub fn message_id(&self) -> &'static str {
         self.message_id
     }
