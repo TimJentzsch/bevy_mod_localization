b986139f60c5e519e3b7c0c75cb71eb1fdfed2a1
Tim Jentzsch <commits@timjen.net>
2024-07-09 13:25:20 +0200

    Add helper functions to get localized paths

--- src/asset/reader.rs
+++ src/asset/reader.rs
@@ -1,4 +1,7 @@
-use std::path::{Path, PathBuf};
+use std::{
+    ffi::OsString,
+    path::{Path, PathBuf},
+};
 
 use async_fs::{read_dir, File};
 use bevy::asset::io::{AssetReader, AssetReaderError, PathStream, Reader};
@@ -23,9 +26,11 @@ pub struct LocalizedReader {
 impl AssetReader for LocalizedReader {
     async fn read<'a>(&'a self, path: &'a Path) -> Result<impl Reader + 'a, AssetReaderError> {
         let full_path = self.root_path.join(path);
-        File::open(&full_path).await.map_err(|e| {
+        let localized_bath = get_localized_path(&full_path, &self.locale);
+
+        File::open(&localized_bath).await.map_err(|e| {
             if e.kind() == std::io::ErrorKind::NotFound {
-                AssetReaderError::NotFound(full_path)
+                AssetReaderError::NotFound(localized_bath)
             } else {
                 e.into()
             }
@@ -87,10 +92,64 @@ impl AssetReader for LocalizedReader {
     }
 }
 
-pub(crate) fn get_meta_path(path: &Path) -> PathBuf {
-    let mut meta_path = path.to_path_buf();
-    let mut extension = path.extension().unwrap_or_default().to_os_string();
-    extension.push(".meta");
-    meta_path.set_extension(extension);
+/// Get the path of the directory which contains the localized assets.
+///
+/// `/flags.png` -> `/flags`
+fn get_localized_dir_path(path: &Path) -> PathBuf {
+    let mut localized_dir_path = path.to_path_buf();
+    localized_dir_path.set_extension(OsString::new());
+    localized_dir_path
+}
+
+/// Transform a path into its localized version for the given locale.
+///
+/// `flags.png` -> `/flags/en-US.png`
+fn get_localized_path(path: &Path, locale: &Locale) -> PathBuf {
+    let extension = path.extension().unwrap_or_default().to_os_string();
+    let mut localized_path = get_localized_dir_path(path);
+
+    localized_path.push(locale.language_id().to_string());
+    localized_path.set_extension(extension);
+
+    localized_path
+}
+
+/// Get the path of the meta file of a localized asset.
+///
+/// `/flags.png` -> `/flags/.meta`
+fn get_meta_path(path: &Path) -> PathBuf {
+    let mut meta_path = get_localized_dir_path(path);
+    meta_path.push(".meta");
     meta_path
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_get_localized_dir_path() {
+        let path = PathBuf::from("/flag.png");
+        let localized_dir_path = get_localized_dir_path(&path);
+
+        assert_eq!(localized_dir_path, PathBuf::from("/flag"));
+    }
+
+    #[test]
+    fn test_get_localized_path() {
+        let path = PathBuf::from("/flag.png");
+        let locale = "en-US".parse().unwrap();
+        let localized_path = get_localized_path(&path, &locale);
+
+        assert_eq!(localized_path, PathBuf::from("/flag/en-US.png"));
+    }
+
+    #[test]
+    fn test_get_meta_path() {
+        let path = PathBuf::from("/flag.png");
+        let meta_path = get_meta_path(&path);
+
+        assert_eq!(meta_path, PathBuf::from("/flag/.meta"));
+    }
 }
